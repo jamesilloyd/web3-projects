56d00ed83fd54c38afbe5cf7ea6112a4cd00f403
James Lloyd <jijlloyd98@gmail.com>
2022-04-25 20:28:00 +0100

    adding hello world project

---
+++ hello-world/scripts/deploy.js
@@ -0,0 +1,16 @@
+async function main() {
+    const HelloWorld = await ethers.getContractFactory("HelloWorld");
+ 
+    // Start deployment, returning a promise that resolves to a contract object
+    const hello_world = await HelloWorld.deploy("Hello World!");
+    console.log("Contract deployed to address:", hello_world.address);
+ }
+ 
+ main()
+   .then(() => process.exit(0))
+   .catch(error => {
+     console.error(error);
+     process.exit(1);
+   });
+
+//    0x11AacaFde38E182A8879c2D4da99c6245584620D
